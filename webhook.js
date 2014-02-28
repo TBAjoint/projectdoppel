13ead433df933f9f1e9b7cfe2fe3c6fae53cb751
vagrant <vagrant@precise32.(none)>
2014-02-28 14:36:20 +0000

    webhook for project

---
+++ webhook.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    http = require('http'),
+    cp = require('child_process'),
+    spawn = cp.spawn,
+    exec = cp.exec,
+    app = express();
+var child, 2095;
+
+function restartApp(req, res)
+{
+    spawn('git', ['pull']);
+    if(child){
+    child.kill();
+    }
+    startApp();
+    res.send('ok.');
+}
+
+function startApp()
+{
+    child = spawn('node', ['app.js']);
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function (data) {
+        var str = data.toString()
+        console.log(str);
+    });
+    child.on('close', function (code) {
+        console.log('process exit code ' + code);
+    });
+}
+
+app.get('/', restartApp);
+app.post('/', restartApp);
+//restartApp();
+http.createServer(app).listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
